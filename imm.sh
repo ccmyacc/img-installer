d8d2d91192318c04578bef96be8f3a44826b77a2
ccmyacc <77372319+ccmyacc@users.noreply.github.com>
2025-08-21 01:22:27 +0800

    Update imm.sh

--- imm.sh
+++ imm.sh
@@ -2,8 +2,8 @@
 mkdir -p imm
 #https://github.com/wukongdaily/AutoBuildImmortalWrt/releases/download/Autobuild-x86-64/immortalwrt-24.10.0-x86-64-generic-squashfs-combined-efi.img.gz
 
-REPO="wukongdaily/AutoBuildImmortalWrt"
-TAG="img-installer"
+REPO="ccmyacc/AutoBuildImmortalWrt"
+TAG="Autobuild-x86-64"
 FILE_NAME="immortalwrt-24.10.2-x86-64-generic-squashfs-combined-efi.img.gz"
 OUTPUT_PATH="imm/immortalwrt.img.gz"
 
